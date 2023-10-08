b56733673261f21131eae3666bd48927d8229dbc
giorgi <giorgi@giorgis-MacBook-Air.local>
2023-10-08 21:06:30 +0400

    made a few changes

---
+++ index.js
@@ -0,0 +1,9 @@
+"use strict";
+function User() {
+    const name = 'giorgi';
+    const age = 37;
+    const isMerried = false;
+    const some = '';
+    console.log(`სახელი: ${name}, ასაკი ${age}`);
+}
+User();
---
+++ index.ts
@@ -0,0 +1,13 @@
+function User (): void {
+    const cars: string[]=['bmw', 'honda', 'mini'];
+
+    const name: string = 'giorgi';
+    const age: number = 37;
+    const isMerried: boolean = false;
+    const some: any ='';
+
+    console.log(`სახელი: ${name}, ასაკი ${age}`);
+    
+}
+
+User();
